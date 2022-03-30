94f9960930f089ec6d9ad88e1ccc0ca827b46d03
kphan32 <kavin.phan@grayscaleapp.com>
2022-03-30 13:38:14 -0400

    Remove trailing white space

--- lib/light-service/organizer/reduce_when.rb
+++ lib/light-service/organizer/reduce_when.rb
@@ -2,7 +2,7 @@ module LightService
   module Organizer
     class ReduceWhen
       extend ScopedReducable
-      
+
       def self.run(organizer, value_key, cases, else_steps)
         lambda do |ctx|
           return ctx if ctx.stop_processing?
