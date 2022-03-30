348043ead7641dc17ab42b042f6ea050aeaad8fd
kphan32 <kavin.phan@grayscaleapp.com>
2022-03-30 13:30:48 -0400

    Fix formatting in reduce_when

--- lib/light-service/organizer/reduce_when.rb
+++ lib/light-service/organizer/reduce_when.rb
@@ -4,16 +4,16 @@ module LightService
       extend ScopedReducable
       
       def self.run(organizer, value_key, cases, else_steps)
-      lambda do |ctx|
-        return ctx if ctx.stop_processing?
+        lambda do |ctx|
+          return ctx if ctx.stop_processing?
 
-        steps = cases[ctx[value_key].to_sym] || else_steps
+          steps = cases[ctx[value_key].to_sym] || else_steps
 
-        ctx = scoped_reduce(organizer, ctx, steps)
+          ctx = scoped_reduce(organizer, ctx, steps)
 
-        ctx
+          ctx
+        end
       end
-    end
     end
   end
 end
